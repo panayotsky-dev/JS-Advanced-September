cf786e5a1c697482f4d3f283a4140e6f24cc27b4
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-09-24 18:04:36 +0300

    Add files via upload

---
+++ 9.DOM/04. JS-Advanced-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,18 @@
+function colorize() {    
+    let rowElements = document.querySelectorAll('tr:nth-of-type(2n) td');
+    rowElements.forEach(x =>{
+        x.style.backgroundColor = 'teal';
+    })
+}
+
+// function colorize() {
+//    let rowElements = document.getElementsByTagName('tr');
+
+//     let rows = Array.from(rowElements);
+//  rows.forEach((x,i)=>{
+//     if (i % 2 != 0){
+//         x.style.backgroundColor = 'teal';
+//     }
+// });
+//   }
+// v2
