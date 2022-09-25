379ff28b70f78a6736edff044464e4b80df779a3
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-09-25 03:59:23 +0300

    Add files via upload

---
+++ 10.DOM-Introduction-EXC/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -0,0 +1,6 @@
+function subtract() {
+    let firstNum = document.getElementById('firstNumber').value
+    let secondNum = document.getElementById('secondNumber').value
+    let sum = Number(firstNum) - Number(secondNum);
+    document.getElementById('result').innerText = sum;
+}
