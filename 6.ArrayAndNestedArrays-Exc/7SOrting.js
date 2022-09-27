95c617abb1727ccde6f0630e80db1597afa0c5de
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-09-27 23:22:16 +0300

    Add files via upload

---
+++ 6.ArrayAndNestedArrays-Exc/7SOrting.js
@@ -0,0 +1,24 @@
+function solve (arr) {
+    let result = [];
+    let sorted = arr.sort((a, b) => a - b);
+    // let smallNum = 0;
+    // let biggestNum = 0;
+  //   arr.forEach(element => {
+  //     while(arr.length > 0) {
+  //     smallNum = arr.shift();
+  //     result.push(smallNum);
+  //     biggestNum = arr.pop();
+  //     result.push(biggestNum);
+  //   }
+  // });
+   
+  while(sorted.length > 0) {
+      let smallNum = sorted.shift();
+      result.push(smallNum);
+      let bigNUm = sorted.pop();
+      result.push(bigNUm)
+    }
+    return result
+  }
+    // solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56])
+   
