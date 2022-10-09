d0afe88a2c287f4229e5ae49412423ea8079902b
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-10-09 15:54:02 +0300

    Add files via upload

---
+++ 11.DOMManipulationsandEvents/04. Mouse-Gradient/app.js
@@ -0,0 +1,14 @@
+function attachGradientEvents() {
+    let gradientElement = document.getElementById('gradient');
+    let resultElement = document.getElementById('result');
+
+    const onMouseOverGradient = (e) => {
+       
+        let percent = Math.floor((e.offsetX / e.target.clientWidth)*100);
+        
+        resultElement.textContent = `${percent}%`;
+    }
+
+
+    gradientElement.addEventListener('mousemove',onMouseOverGradient);
+}
