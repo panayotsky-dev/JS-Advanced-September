9c42cccc71f1e5c1a23c5e204cd9e4191b761b06
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-10-17 00:52:38 +0300

    Update 1.SortArray.js

--- 14.Advanced-Functions-Exc/1.SortArray.js
+++ 14.Advanced-Functions-Exc/1.SortArray.js
@@ -1,5 +1,5 @@
 
-//v1
+//v1 simple
 function solve(array,argument){
 //     if(argument == 'asc'){
 //         array.sort((a,b) => a - b)
@@ -10,13 +10,16 @@ function solve(array,argument){
 //     }
 // }
 
-// v2
+// v2 object
 let type = {
     asc: (a,b) => a - b,
     desc: (a,b) => b - a,
 };
 array.sort(type[argument])
 return array
+    
+// v3 one line version
+// return argument === 'asc' ? array.sort((a,b) => a-b) : array.sort((a,b) => b -a);
 }
 
 console.log(solve([14, 7, 17, 6, 8], 'asc'))
