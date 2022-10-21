5944f6e019d57d86b3dc51505662be550b2caa3b
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-10-22 01:34:22 +0300

    Add files via upload

---
+++ 21.Еxam-Prep/03. Book-selection/solution.test.js
@@ -0,0 +1,10 @@
+let {isGenreSuitable,isItAffordable,suitableTitles} = require('/solution')
+let {assert} = require('chai')
+describe("test", function(){
+    describe("Todo", function(){
+
+        it("Todo...", function(){
+            assert.equal(1,1)
+        })
+    })
+})
