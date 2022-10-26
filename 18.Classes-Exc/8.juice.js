20948970330b7259f772db9e949c95640793b68c
panayotsky.dev <104060829+panayotsky-dev@users.noreply.github.com>
2022-10-26 13:17:53 +0300

    Add files via upload

---
+++ 18.Classes-Exc/8.juice.js
@@ -0,0 +1,30 @@
+function juiceFlavors(input) {
+    const juices = new Map();
+    const producedBottles = new Map();
+  
+    input.forEach(d => {
+      let [juice, quantity] = d.split(' => ');
+      quantity = +quantity;
+  
+      if (!juices.has(juice)) {
+        juices.set(juice, 0);
+      }
+  
+      juices.set(juice, juices.get(juice) + quantity);
+  
+      if (juices.get(juice) >= 1000) {
+        producedBottles.set(juice, Math.floor(juices.get(juice) / 1000));
+      }
+    });
+  
+    producedBottles.forEach((q, n) => console.log(`${n} => ${q}`));
+  }
+
+  juiceFlavors(['Kiwi => 234',
+  'Pear => 2345',
+  'Watermelon => 3456',
+  'Kiwi => 4567',
+  'Pear => 5678',
+  'Watermelon => 6789']
+  )
+  
